f99dae21c8942adeccc95ce2a92167c8f0953b0f
christopher-christofi <christopherlchristofi@outlook.com>
2021-04-08 20:48:51 +0100

    Added model schema defintions

---
+++ app/models/model.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+const CiteStashSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  link: String,
+  description: String,
+  notes: String
+}, {
+  // Track when created and updated
+  timestamps: true
+});
+
+module.exports = mongoose.model('CiteStash', CiteStashSchema);
